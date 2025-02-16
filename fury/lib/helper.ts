d5720a41ac0755906fc8b6ac7b4cede74e45e9be
nyel-101 <dandevjr@gmail.com>
2025-02-16 06:25:20 +0000

    feat(core): refactor middleware routes

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7f248f375e6445f991116a0b9a33b340 = () => {
-  return '2026-04-30T20:50:42Z';
+const node_d128589601f045589eff89651b9bbf1a = () => {
+  return '2025-02-16T06:25:20Z';
 };
