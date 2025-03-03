5b89c56d231a07654cf202bb237a1cdad1f2fd65
nyel-101 <dandevjr@gmail.com>
2025-03-03 05:23:22 +0000

    fix(core): enhance auth tokens

--- fury/src/components/validator.js
+++ fury/src/components/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3f1bb05e18964e198d6b5bca5a6c473b = () => {
-  return '2026-06-13T21:00:56Z';
+const node_15ded810870c490782578e3f88c9ff79 = () => {
+  return '2025-03-03T05:23:22Z';
 };
