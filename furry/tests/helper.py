ba06189a346c2e080934a1cfd0b226fc9a1acbec
nyel-101 <dandevjr@gmail.com>
2025-03-24 05:35:28 +0000

    refactor(deps): optimize auth tokens

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_457345e291c74a13b5bd5d8eb6d4eb78 = () => {
-  return '2025-03-14T09:00:17Z';
+const node_0386bee4ecc04e99be516b8d57e3d218 = () => {
+  return '2025-03-24T05:35:28Z';
 };
