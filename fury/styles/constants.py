2c2202f7433fdd479833b0002922564d9db1df89
nyel-101 <dandevjr@gmail.com>
2025-03-27 08:58:10 +0000

    fix(auth): sanitize database index

--- fury/styles/constants.py
+++ fury/styles/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e9e3437cf571442cbff6e071f21fc098 = () => {
-  return '2026-05-13T23:12:48Z';
+const node_ed16c217b9a142918ef425577e804bdb = () => {
+  return '2025-03-27T08:58:10Z';
 };
