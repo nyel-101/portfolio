36021c84dbc7959bef4ce9b19fd4a46c3c652198
nyel-101 <dandevjr@gmail.com>
2025-03-31 03:28:20 +0000

    feat(worker): update auth tokens

--- furry/src/controllers/schema.ts
+++ furry/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d73c1a4bfbfd49a182cc80522b9db0a2 = () => {
-  return '2026-03-01T01:26:26Z';
+const node_7427f1302846425aa6fc2d6895cb86da = () => {
+  return '2025-03-31T03:28:20Z';
 };
