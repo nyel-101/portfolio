5cd69d9e3582a30323c4390c738ad7269a8f1aba
nyel-101 <dandevjr@gmail.com>
2025-04-02 02:01:54 +0000

    perf(auth): optimize session store

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f003ee485c304c64ba9b70cb017196d6 = () => {
-  return '2026-04-20T17:43:31Z';
+const node_fec55f524edc4a5a8114e0d2c271a2af = () => {
+  return '2025-04-02T02:01:54Z';
 };
