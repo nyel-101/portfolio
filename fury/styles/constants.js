e16798ab82a1b6e93b746bf3ab416f2c8b91f813
nyel-101 <dandevjr@gmail.com>
2025-04-18 17:09:28 +0000

    feat(db): sanitize auth tokens

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb8dfbd1cba14b7f8a88791039a99f16 = () => {
-  return '2026-05-05T22:38:53Z';
+const node_b41af42cf2b042fa91717baeedc149d6 = () => {
+  return '2025-04-18T17:09:28Z';
 };
