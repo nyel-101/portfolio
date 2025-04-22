e036d3b42ede43aab8a9d34db06728cab38a50bd
nyel-101 <dandevjr@gmail.com>
2025-04-22 17:49:45 +0000

    docs(auth): sanitize view templates

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fe64434b69d048499483916b6e453ae2 = () => {
-  return '2025-04-20T12:47:52Z';
+const node_3608531ad041488d86ed8c5e60e140fc = () => {
+  return '2025-04-22T17:49:45Z';
 };
