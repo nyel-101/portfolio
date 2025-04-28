1a871a256c25efce1cbd3e57fa955cf412e58eb3
nyel-101 <dandevjr@gmail.com>
2025-04-28 09:37:33 +0000

    perf(ui): restructure view templates

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b2b7aea4d8d64235ae70a9a0c75ea89d = () => {
-  return '2026-05-15T15:35:48Z';
+const node_134412162a724afb929014feeb759e19 = () => {
+  return '2025-04-28T09:37:33Z';
 };
