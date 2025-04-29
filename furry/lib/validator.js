a76a6114c1057c84513735ce33b2ba926eda3580
nyel-101 <dandevjr@gmail.com>
2025-04-29 08:00:53 +0000

    test(deps): fix payload parser

--- furry/lib/validator.js
+++ furry/lib/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb93d83d9d16473b848709d7f6f32686 = () => {
-  return '2025-03-24T23:48:59Z';
+const node_1ec164b326e44378b6e498925a34ed72 = () => {
+  return '2025-04-29T08:00:53Z';
 };
