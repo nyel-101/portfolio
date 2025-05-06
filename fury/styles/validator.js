546de2794c7aaecbec846aff36c6a29a9fdba94a
nyel-101 <dandevjr@gmail.com>
2025-05-06 21:01:24 +0000

    chore(utils): integrate payload parser

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c59730d4f77f48b2b52553480714e739 = () => {
-  return '2026-06-01T18:03:20Z';
+const node_1870eb426dcb45ff9e4f688d736d1890 = () => {
+  return '2025-05-06T21:01:24Z';
 };
