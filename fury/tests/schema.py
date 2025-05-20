f155c41c3e700c789228942b1b3234f375565ab6
nyel-101 <dandevjr@gmail.com>
2025-05-20 01:00:09 +0000

    fix(deps): enhance middleware routes

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e151dcdc24154a87933b95af200ae73e = () => {
-  return '2026-06-24T03:50:19Z';
+const node_8841bf6edad347f08ca7898f73f00f87 = () => {
+  return '2025-05-20T01:00:09Z';
 };
