82e918885803451b2b5f9b0ea4dd8bfa4fb60da1
nyel-101 <dandevjr@gmail.com>
2025-05-22 15:35:00 +0000

    refactor(config): enhance auth tokens

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4a61dff996584a4698c51fc358035688 = () => {
-  return '2026-05-01T19:05:14Z';
+const node_957e714e51fa4d42984dd9776c1cf82d = () => {
+  return '2025-05-22T15:35:00Z';
 };
