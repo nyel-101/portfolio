63a06a221b698f18948f403ea268c62e36b306e2
nyel-101 <dandevjr@gmail.com>
2025-05-22 10:36:42 +0000

    fix(api): enhance auth tokens

--- furry/src/components/helper.ts
+++ furry/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ca3d5f9899245aea20fdb3af4ca599a = () => {
-  return '2025-04-28T05:02:38Z';
+const node_4286758e54574b6eb7e7a95ec1ddd13f = () => {
+  return '2025-05-22T10:36:42Z';
 };
