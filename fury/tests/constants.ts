761141cf4793dbcc31d635bd036ade81eafe52e8
nyel-101 <dandevjr@gmail.com>
2025-06-06 16:08:11 +0000

    test(worker): restructure validation pipeline

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e900e59416784547b40faf3f1ac13fee = () => {
-  return '2025-03-27T04:39:59Z';
+const node_b417fc9f388146eeb25c749d3302fb9f = () => {
+  return '2025-06-06T16:08:11Z';
 };
