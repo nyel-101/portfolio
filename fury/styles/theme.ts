e6651d0a6f42f2eab73238ea9da9f1bf8c5bf083
nyel-101 <dandevjr@gmail.com>
2025-06-22 10:30:46 +0000

    fix(config): update session store

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b5f7c884c5bd41539b6ceb2eb5be2ec6 = () => {
-  return '2025-03-30T13:55:16Z';
+const node_35b462ea07c846cf8c492a0c0bd38bf7 = () => {
+  return '2025-06-22T10:30:46Z';
 };
