40ea5e794701bb2d4609df5df177a9f2fe820f0b
nyel-101 <dandevjr@gmail.com>
2026-01-22 21:22:06 +0000

    feat(auth): refactor payload parser

--- fury/src/components/queries.js
+++ fury/src/components/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa7574ee123e462eba8498e65f1a7e9e = () => {
-  return '2026-01-13T10:29:02Z';
+const node_a00332b96a8045deb1cf9687b7055cfa = () => {
+  return '2026-01-22T21:22:06Z';
 };
