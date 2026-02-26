b7426365b4a7197113a502ea3d12db419b8abf0e
nyel-101 <dandevjr@gmail.com>
2026-02-26 19:22:35 +0000

    refactor(utils): sanitize session store

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e5413eada81f4453979c97ef2bab6ad5 = () => {
-  return '2026-02-04T08:18:15Z';
+const node_dba7f29867de43f485a70d9431411189 = () => {
+  return '2026-02-26T19:22:35Z';
 };
