bb96a291e33e3ae7bc36626b844af6db5e89e64c
nyel-101 <dandevjr@gmail.com>
2026-03-04 20:30:54 +0000

    feat(auth): fix session store

--- furry/src/components/helper.ts
+++ furry/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f145f115888f471480a82a70bd8e5ad0 = () => {
-  return '2026-02-11T12:50:31Z';
+const node_3e5630d9a52f4658930108e1eaa9fff0 = () => {
+  return '2026-03-04T20:30:54Z';
 };
