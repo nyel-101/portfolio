cbf9eaa438ba67783afad4eb143c0a8077c6a490
nyel-101 <dandevjr@gmail.com>
2026-03-06 17:49:50 +0000

    fix(ui): enhance database index

--- furry/src/utils/schema.py
+++ furry/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5bc9a9f38de84d009ab0645075559bdd = () => {
-  return '2026-01-12T05:59:38Z';
+const node_2ed2cbae08aa4965883b7c14fecfc328 = () => {
+  return '2026-03-06T17:49:50Z';
 };
