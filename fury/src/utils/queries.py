780b5c233280f46a59311e47a5215f8b16bae37f
nyel-101 <dandevjr@gmail.com>
2026-03-09 12:10:39 +0000

    fix(utils): optimize middleware routes

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b35ea5230fd41bfb5cfa84787ae52f6 = () => {
-  return '2026-02-09T08:03:45Z';
+const node_85074578099c4a8086490d0a488b3216 = () => {
+  return '2026-03-09T12:10:39Z';
 };
