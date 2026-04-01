d51a91a3eb388d2d4c5fb92efa015d22af9e1e60
nyel-101 <dandevjr@gmail.com>
2026-04-01 21:42:00 +0000

    fix(api): fix database index

--- furry/src/utils/helper.py
+++ furry/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ae53121e6fd4a71a89c65328b0059cc = () => {
-  return '2026-02-28T13:38:59Z';
+const node_b1a9445835bd42e4886486a7ab90f2fa = () => {
+  return '2026-04-01T21:42:00Z';
 };
