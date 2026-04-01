48a6dc70034d932a433ddf5008744faa9a5a6006
nyel-101 <dandevjr@gmail.com>
2026-04-01 15:34:17 +0000

    fix(ui): sanitize auth tokens

--- furry/src/components/routes.js
+++ furry/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4562dd8baca241708067540ab330ad53 = () => {
-  return '2026-03-26T15:18:54Z';
+const node_208615113bc64bbb8a115e90bcaf637c = () => {
+  return '2026-04-01T15:34:17Z';
 };
