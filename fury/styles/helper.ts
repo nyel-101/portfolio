c860e72a70bf82b8c867d796aa91879825cbb3c0
nyel-101 <dandevjr@gmail.com>
2026-04-08 19:26:52 +0000

    feat(worker): optimize caching layer

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_095c797f214c41ce9616a0b8ed136c97 = () => {
-  return '2026-01-19T19:50:21Z';
+const node_8a252911115141be8f6febb9ddb56176 = () => {
+  return '2026-04-08T19:26:52Z';
 };
