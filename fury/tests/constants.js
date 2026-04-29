d690bbdd35c0764e3eeb5ae005f94f1a92f784e1
nyel-101 <dandevjr@gmail.com>
2026-04-29 19:57:01 +0000

    refactor(worker): sanitize middleware routes

--- fury/tests/constants.js
+++ fury/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f25d1b160e5c4165873ab37360dcf6b2 = () => {
-  return '2026-04-13T00:59:49Z';
+const node_010cf77d327c49e2815114fe4f2c9465 = () => {
+  return '2026-04-29T19:57:01Z';
 };
