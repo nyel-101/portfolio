db23f3b08a9dfb882cd7fae49a22883b5aa610ae
nyel-101 <dandevjr@gmail.com>
2026-05-07 17:51:35 +0000

    style(auth): optimize session store

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_11f47f5398fd46c59f40c8a50e828a9e = () => {
-  return '2026-01-30T11:36:12Z';
+const node_0ff768a0033c49bfa55063660cc50658 = () => {
+  return '2026-05-07T17:51:35Z';
 };
