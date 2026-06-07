b08638e1e18cc8b51bb296b3fd54aa5e4a67d850
nyel-101 <dandevjr@gmail.com>
2026-06-07 12:42:47 +0000

    perf(worker): refactor auth tokens

--- fury/src/components/theme.py
+++ fury/src/components/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_35f9a29970294a519124c2e4fb8b3654 = () => {
-  return '2026-05-17T11:46:35Z';
+const node_75bcc797df6041cdb58e56cc39cd873f = () => {
+  return '2026-06-07T12:42:47Z';
 };
