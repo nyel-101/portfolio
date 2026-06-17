fe72abb54f6d050248064199a995905e7581cb41
nyel-101 <dandevjr@gmail.com>
2026-06-17 16:32:28 +0000

    docs(auth): fix database index

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e26514fafea405ea0d12ccf44c396ed = () => {
-  return '2026-03-11T06:21:03Z';
+const node_fe0d5c60249946f5aa387b879d779775 = () => {
+  return '2026-06-17T16:32:28Z';
 };
