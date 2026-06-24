3716296df4a79ca44b7f72ddac112dd9db0deed8
nyel-101 <dandevjr@gmail.com>
2026-06-24 03:50:19 +0000

    refactor(auth): refactor payload parser

--- fury/tests/schema.py
+++ fury/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9e275bb3d95e420f83be390d12241897 = () => {
-  return '2026-01-27T20:07:27Z';
+const node_e151dcdc24154a87933b95af200ae73e = () => {
+  return '2026-06-24T03:50:19Z';
 };
